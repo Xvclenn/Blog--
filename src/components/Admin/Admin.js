30683127c67f468f78ba63a5d916038db0ac3436
Xvclenn <benhhslen@gmail.com>
2023-01-24 14:09:41 +0800

    ...

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,15 @@
+export function Admin() {
+    return (
+        <>
+            <div>
+                <h1>This Is Admin Area</h1>
+            </div>
+
+            <div>
+                <input placeholder='Title of news' type='text' />
+                <textarea placeholder='Informations of news' />
+                <input placeholder='Title of news' />
+            </div>
+        </>
+    );
+}
---
+++ src/components/Client/Client.js
@@ -0,0 +1,7 @@
+export function Client() {
+    return (
+        <div>
+            <h1>This Is Client Area</h1>
+        </div>
+    );
+}
